6620f7f8f7ab13276486af5aeb5ef04a51aa376d
septor <patrickweaver@gmail.com>
2016-09-30 23:43:36 -0400

    "bar" support added

--- ClassPower.lua
+++ ClassPower.lua
@@ -23,7 +23,9 @@ end)
 local class, classID = select(2, UnitClass("player"))
 local color = RAID_CLASS_COLORS[class]
 
-local text = cpFrame:CreateFontString(nil, "OVERLAY")
+local text = cpFrame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+local pointCharacter = 'X'
+local depletedCharacter = 'x'
 
 text:SetFont("Interface\\AddOns\\ClassPower\\Fonts\\MUNIRG__.TTF", 16)
 text:SetTextColor(color.r, color.g, color.b, 1)
@@ -31,9 +33,14 @@ text:SetPoint("CENTER", 0, 0)
 
 cpFrame:RegisterEvent("UNIT_POWER")
 
+local function repeats(s, n)
+	return n > 0 and s .. repeats(s, n-1) or ""
+end
+
 local function UpdateText(self, event)
 	if classID == 2 then
-		displayText = UnitPower("player", 9)
+		pointCount = UnitPower("player", 9)
+		maxPoints = 5
 
 	elseif classID == 6 then
 		local numReady = 0
@@ -43,26 +50,37 @@ local function UpdateText(self, event)
 				numReady = numReady + 1
 			end
 		end
-		displayText = numReady
+		pointCount = numReady
+		maxPoints = 6
 
 	elseif classID == 4 then
-		displayText = GetComboPoints("player", "target")
+		pointCount = GetComboPoints("player", "target")
+		maxPoints = UnitPowerMax("player", 4)
 
 	elseif classID == 10 then
-		displayText = UnitPower("player", 12)
+		pointCount = UnitPower("player", 12)
+		maxPoints = UnitPowerMax("player", 12)
 
 	elseif classID == 8  then
-		displayText = UnitPower("player", 16)
+		pointCount = UnitPower("player", 16)
+		maxPoints = 4
 
 	elseif classID == 11 then
-		displayText = GetComboPoints("player", "target")
+		pointCount = GetComboPoints("player", "target")
+		maxPoints = UnitPowerMax("player", 4)
 
 	elseif classID == 9 then
-		displayText = UnitPower("player", 7)
+		pointCount = UnitPower("player", 7)
+		maxPoints = UnitPowerMax("player", 7)
 
  	end
 
-	text:SetText(displayText)
+	leftOver = maxPoints - pointCount
+
+	active = repeats(pointCharacter, pointCount)
+	depleted = repeats(depletedCharacter, leftOver)
+
+	text:SetText(active..depleted)
 end
 
 cpFrame:SetScript("OnEvent", UpdateText)
